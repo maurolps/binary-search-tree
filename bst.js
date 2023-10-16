4295611c7e499372630470f9343e261be1534457
maurolps <boroengenharia@gmail.com>
2023-10-16 19:00:56 -0300

    Added insert function

--- bst.js
+++ bst.js
@@ -1,16 +1,3 @@
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-  if (node === null) {
-    return;
-  }
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-  }
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-  }
-};
-
 function Node ( data, left = null, right = null) {
 	return { data, left, right }
 }
@@ -25,9 +12,41 @@ function buildTree(arr, start, end) {
   return root;
 }
 
-function Tree (arr = [ 1, 2, 3, 4, 5, 6, 7]) {
+function Tree (arr = [ 1, 2, 3, 4, 5, 6, 7, 8]) {
   const root = buildTree(arr, 0, 7);
-  prettyPrint(root);
+  
+  const insert = (value, treeRoot = root) => {
+    // const newNode = Node(value);
+    if (treeRoot == null) {
+      treeRoot = Node(value);
+      return treeRoot;
+    }
+    if (value < treeRoot.data) {
+      treeRoot.left = insert(value, treeRoot.left);
+    } else if (value > treeRoot.data) {
+      treeRoot.right = insert(value, treeRoot.right);
+    }
+    return treeRoot;
+  }
+
+  // Print a structured format of the Tree to console.
+const prettyPrint = (node = root, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+  return {insert, prettyPrint}
 }
 
-Tree();
+const bsTree = Tree();
+bsTree.prettyPrint();
+// bsTree.insert(3);
+// bsTree.prettyPrint();
